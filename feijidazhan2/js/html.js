60aa146ca8ba65727f7446a1c233718334ab99af
unknown <you@example.com>
2017-10-21 09:01:02 +0800

    飞机大战第二次上传

---
+++ feijidazhan2/js/html.js
@@ -0,0 +1,1 @@
+"use strict";onload=function(){for(var e=document.getElementById("list"),t=e.getElementsByTagName("li"),n=0;n<t.length;n++)t[n].onclick=function(){e.parentNode.removeChild(e),myPlane.fireInterval=this.value,gameEngine.init().start()}};
